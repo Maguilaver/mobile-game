5f4e65a8ac8bde1bbdfbfe0a86f7215e7cc06e06
Maguilaver <heliopereira159@gmail.com>
2021-10-12 21:54:29 -0300

    Off-on sound e organização menu

--- Assets/Scripts/ControladorLoja.cs
+++ Assets/Scripts/ControladorLoja.cs
@@ -5,50 +5,34 @@ using UnityEngine.UI;
 
 public class ControladorLoja : MonoBehaviour
 {
-    //dinheiro
     public Text moedaLoja;
 
     private int moedinhas;
 
-    //painel
     public GameObject semDinheiro;
-
-    
 
 
     void Start()
     {
         moedinhas = PlayerPrefs.GetInt("totalScore"); //relacionar variavel da loja com o save das moedas
-
-       //ItemLoja.acesso.Comprado = PlayerPrefs.GetInt("Comprado", 1);
+        
+        
     }
     void Update()
     {
         moedaLoja.text = PlayerPrefs.GetInt("totalScore").ToString();
-
-      /*  if(ItemLoja.acesso.Comprado == 1)
-        {
-            ItemLoja.acesso.item.SetActive(false);
-        }
-
-        if(ItemLoja.acesso.Comprado == 2)
-        {
-            ItemLoja.acesso.item.SetActive(true);
-        }
-      */
     }
     public void Comprar()
     {
         if (moedinhas >= ItemLoja.acesso.preço)
         {
-            //salvar
-          //ItemLoja.acesso.Comprado = 2;
-          //PlayerPrefs.SetInt("Comprado", ItemLoja.acesso.Comprado);
-           //PlayerPrefs.Save();
             moedinhas = moedinhas - ItemLoja.acesso.preço;
             PlayerPrefs.SetInt("totalScore", moedinhas);
             PlayerPrefs.Save();
+
             ItemLoja.acesso.item.SetActive(true);
+
+
 
         }
         else
